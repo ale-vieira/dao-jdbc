2e76a1bebb76a659dab9df25a7c970eadde34bfc
Alexandre Vieira <alexandrevieira76@gmail.com>
2022-09-29 15:23:10 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 // a classe retorna um método do tipo da interface, mas internamente instancia uma implementação.
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
